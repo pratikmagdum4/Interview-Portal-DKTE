769fa2b473c30b41af094f4f1c0ea40ce1208182
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-05-03 20:28:01 +0530

    nav studenthome students

--- src/pages/StudentPages/StudentHome.jsx
+++ src/pages/StudentPages/StudentHome.jsx
@@ -2,9 +2,9 @@
 import React, { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import { StudentHomePicture } from '@/assets/index.js';
+import '@/App.css'
 
-
-const BUTTON_CLASS = 'bg-yellow-400 text-white px-4 py-2 rounded';
+const BUTTON_CLASS = 'bg-yellow-400 text-white px-4 py-3 rounded m-2';
 const HOVER_CLASS = 'hover:bg-zinc-700';
 const PROFILE_MENU_CLASS = 'absolute right-0 top-full bg-white text-black w-32 p-2 rounded hidden';
 
@@ -13,12 +13,12 @@ const MainContent = () => {
    
     return (
         <>
-        <div className="flex-1 p-10 justify-center">
-            <div className="flex justify-center items-center ">
-                <div className="flex space-x-4 just">
+            <div className=" flex-1 p-10 justify-center items-center">
+                <div className=" flex justify-center items-center ">
+                <div className="StudentHome flex  space-x-4 ">
                     <button className={BUTTON_CLASS}>Today's Interview</button>
                     <button className={BUTTON_CLASS}>Upcoming Interview</button>
-                    <button className={BUTTON_CLASS}>Interviews History</button>
+                        <button className={`${BUTTON_CLASS} mr-2`}>Interviews History</button>
                 </div>
             </div>
                 <div className="mt-10 flex justify-center">
